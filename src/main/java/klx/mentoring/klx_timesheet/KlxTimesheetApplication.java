4719421d8e30cdffa5f3c5e83b27e8a3aa3a6a80
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-24 18:55:26 +0000

    feat: Configure Spring Boot project

---
+++ src/main/java/klx/mentoring/klx_timesheet/KlxTimesheetApplication.java
@@ -0,0 +1,13 @@
+package klx.mentoring.klx_timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlxTimesheetApplication {	
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlxTimesheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/klx/mentoring/klx_timesheet/KlxTimesheetApplicationTests.java
@@ -0,0 +1,12 @@
+package klx.mentoring.klx_timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlxTimesheetApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
