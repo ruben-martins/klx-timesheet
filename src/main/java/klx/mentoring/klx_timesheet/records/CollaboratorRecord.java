0da9711e8b136ddec027e9234b710a0a677ff71d
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-24 18:55:54 +0000

    feat: Create Collaborator record

---
+++ src/main/java/klx/mentoring/klx_timesheet/records/CollaboratorRecord.java
@@ -0,0 +1,10 @@
+package klx.mentoring.klx_timesheet.records;
+
+public record CollaboratorRecord(
+    Long id,
+    String name,
+    String lastName,
+    String businessUnity,
+    String position
+) {
+}
