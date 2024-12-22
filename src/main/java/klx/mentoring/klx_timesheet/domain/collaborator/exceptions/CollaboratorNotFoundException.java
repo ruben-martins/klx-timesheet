9ece87a8771629b463482ec630c28848ac1ab7dc
orlando-santos <santos.orlando.martin@gmail.com>
2024-12-22 15:28:02 +0000

    :wastebasket: Delete CollaboratorNotFoundException

--- src/main/java/klx/mentoring/klx_timesheet/domain/collaborator/exceptions/CollaboratorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package klx.mentoring.klx_timesheet.domain.collaborator.exceptions;
-
-public class CollaboratorNotFoundException extends RuntimeException {
-    public CollaboratorNotFoundException(String message) {
-        super(message);
-    }
-}
