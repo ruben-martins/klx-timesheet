c0cd581f3ad30c6f217e1490e845eb925913bf7d
orlando-santos <santos.orlando.martin@gmail.com>
2024-12-22 15:29:04 +0000

    :construction: Add validation methods

--- src/main/java/klx/mentoring/klx_timesheet/domain/collaborator/model/Collaborator.java
+++ src/main/java/klx/mentoring/klx_timesheet/domain/collaborator/model/Collaborator.java
@@ -1,8 +1,9 @@
 package klx.mentoring.klx_timesheet.domain.collaborator.model;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
-
 public record Collaborator(
     UUID id,
     String name,
@@ -10,4 +11,51 @@ public record Collaborator(
     String email,
     LocalDate hireDate,
     String position
-) { }
+) {
+
+    public Map<String, String> validate() {
+        Map<String, String> errors = new HashMap<>();
+        validateName(errors);
+        validateLastName(errors);
+        validateEmail(errors);
+        validateHireDate(errors);
+        return errors;
+    }
+
+    private void validateName(Map<String, String> errors) {
+        if (name == null || name.trim().isEmpty() || name.length() < 2 || name.length() > 100) {
+            errors.put("name", "Name must be between 2 and 100 characters and cannot be null or empty.");
+        }
+    }
+
+    private void validateLastName(Map<String, String> errors) {
+        if (lastName == null || lastName.trim().isEmpty()) {
+            errors.put("lastName", "Last name cannot be null or empty.");
+        }
+    }
+
+    private void validateEmail(Map<String, String> errors) {
+        if (email == null || !email.contains("@") || email.trim().isEmpty()) {
+            errors.put("email", "Email must be valid and cannot be null or empty.");
+        }
+    }
+
+    private void validateHireDate(Map<String, String> errors) {
+        if (hireDate == null) {
+            errors.put("hireDate", "Hire date cannot be null.");
+            return;
+        }
+
+        try {
+            // Verificar si hireDate tiene un formato válido
+            LocalDate parsedDate = LocalDate.parse(hireDate.toString());
+
+            // Validar si la fecha está en el futuro
+            if (parsedDate.isAfter(LocalDate.now())) {
+                errors.put("hireDate", "Hire date must be in the past or today.");
+            }
+        } catch (Exception e) {
+            errors.put("hireDate", "Hire date format is invalid.");
+        }
+    }
+}
