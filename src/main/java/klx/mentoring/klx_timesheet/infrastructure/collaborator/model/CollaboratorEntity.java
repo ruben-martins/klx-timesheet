b2307e6302219fe3b7a3e1742949eb644fc6f696
orlando-santos <santos.orlando.martin@gmail.com>
2025-01-04 13:25:56 +0000

    :construction: Add hashCode and equals methods

--- src/main/java/klx/mentoring/klx_timesheet/infrastructure/collaborator/model/CollaboratorEntity.java
+++ src/main/java/klx/mentoring/klx_timesheet/infrastructure/collaborator/model/CollaboratorEntity.java
@@ -92,4 +92,35 @@ public class CollaboratorEntity {
         this.position = position;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CollaboratorEntity other = (CollaboratorEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
 }
