22226a7a009a04c02df55260b31e4ce0eaa7b7e4
orlando-santos <santos.orlando.martin@gmail.com>
2025-01-04 13:42:54 +0000

    :sparkles: Create Util class

---
+++ src/main/java/klx/mentoring/klx_timesheet/infrastructure/utils/Util.java
@@ -0,0 +1,44 @@
+package klx.mentoring.klx_timesheet.infrastructure.utils;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import klx.mentoring.klx_timesheet.domain.businessunit.model.BusinessUnit;
+import klx.mentoring.klx_timesheet.domain.collaborator.model.Collaborator;
+import klx.mentoring.klx_timesheet.infrastructure.businessunit.model.BusinessUnityEntity;
+import klx.mentoring.klx_timesheet.infrastructure.collaborator.model.CollaboratorEntity;
+
+public final class Util {
+
+    public static Collaborator colloboratorEntitytoRecord(CollaboratorEntity collaborator) {
+        return new Collaborator(
+                collaborator.getId(),
+                collaborator.getName(),
+                collaborator.getLastName(),
+                collaborator.getEmail(),
+                collaborator.getHireDate(),
+                collaborator.getPosition()
+        );
+    }
+
+    public static CollaboratorEntity collaboratorRecordtoEntity(Collaborator collaborator) {
+        CollaboratorEntity collaboratorEntity = new CollaboratorEntity();
+        collaboratorEntity.setName(collaborator.name());
+        collaboratorEntity.setLastName(collaborator.lastName());
+        collaboratorEntity.setEmail(collaborator.email());
+        collaboratorEntity.setHireDate(collaborator.hireDate());
+        collaboratorEntity.setPosition(collaborator.position());
+        return collaboratorEntity;
+    }
+
+    public static BusinessUnit businessUnityEntityToRecord(BusinessUnityEntity businessUnityEntity){
+        Set<Collaborator> collaborators = businessUnityEntity.getCollaborators().stream().
+                            map(bUE -> colloboratorEntitytoRecord(bUE)).
+                            collect(Collectors.toSet());
+        
+        return new BusinessUnit(businessUnityEntity.getId(),
+                                businessUnityEntity.getName(),
+                                collaborators);
+    }
+
+}
