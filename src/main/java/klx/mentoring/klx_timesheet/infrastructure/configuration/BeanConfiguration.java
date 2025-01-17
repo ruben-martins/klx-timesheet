f71b27c0e89d2ac4911654b85ffddddf3e656f5b
orlando-santos <santos.orlando.martin@gmail.com>
2025-01-17 18:36:28 +0000

    :construction: Create BeanConfiguration classs

--- src/main/java/klx/mentoring/klx_timesheet/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/klx/mentoring/klx_timesheet/infrastructure/configuration/BeanConfiguration.java
@@ -1,20 +1,26 @@
-package klx.mentoring.klx_timesheet.infrastructure.businessunit.configuration;
+package klx.mentoring.klx_timesheet.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import klx.mentoring.klx_timesheet.domain.businessunit.ports.persistence.BusinessUnitRepositoryPort;
 import klx.mentoring.klx_timesheet.domain.businessunit.ports.service.BusinessUnitServicePort;
+import klx.mentoring.klx_timesheet.domain.businessunit.service.BusinessUnitServiceImpl;
+import klx.mentoring.klx_timesheet.domain.collaborator.ports.interfaces.CollaboratorServicePort;
 import klx.mentoring.klx_timesheet.domain.collaborator.ports.persistence.CollaboratorRepositoryPort;
-import klx.mentoring.klx_timesheet.domain.businessunit.adapter.service.BusinessUnitServiceImpl;
-
+import klx.mentoring.klx_timesheet.domain.collaborator.service.CollaboratorServiceImpl;
 
 @Configuration
-public class BusinessUnitConfig {
+public class BeanConfiguration {
+
+    @Bean
+    CollaboratorServicePort CollaboratorService(CollaboratorRepositoryPort collaboratorRepositoryPort) {
+        return new CollaboratorServiceImpl(collaboratorRepositoryPort);
+    }
 
     @Bean
     BusinessUnitServicePort BusinessService(BusinessUnitRepositoryPort businessUnitRepositoryPort,
-                                            CollaboratorRepositoryPort collaboratorRepositoryPort) {
+            CollaboratorRepositoryPort collaboratorRepositoryPort) {
         return new BusinessUnitServiceImpl(businessUnitRepositoryPort, collaboratorRepositoryPort);
     }
 }
