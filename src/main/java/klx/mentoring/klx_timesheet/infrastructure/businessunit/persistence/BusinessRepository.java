b52b68a04a5a203bd982f75159d745cf921f59b7
orlando-santos <santos.orlando.martin@gmail.com>
2025-01-17 18:42:39 +0000

    :recycle: Refactor methods

--- src/main/java/klx/mentoring/klx_timesheet/infrastructure/businessunit/persistence/BusinessRepository.java
+++ src/main/java/klx/mentoring/klx_timesheet/infrastructure/businessunit/persistence/BusinessRepository.java
@@ -1,8 +1,6 @@
 package klx.mentoring.klx_timesheet.infrastructure.businessunit.persistence;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -14,12 +12,11 @@ import org.springframework.stereotype.Repository;
 import klx.mentoring.klx_timesheet.domain.businessunit.exceptions.NotFoundCollaboratorException;
 import klx.mentoring.klx_timesheet.domain.businessunit.model.BusinessUnit;
 import klx.mentoring.klx_timesheet.domain.businessunit.ports.persistence.BusinessUnitRepositoryPort;
-import klx.mentoring.klx_timesheet.domain.collaborator.model.Collaborator;
 import klx.mentoring.klx_timesheet.infrastructure.businessunit.model.BusinessUnityEntity;
 import klx.mentoring.klx_timesheet.infrastructure.collaborator.model.CollaboratorEntity;
 import klx.mentoring.klx_timesheet.infrastructure.collaborator.persistence.JpaCollaboratorRepository;
 
-import static klx.mentoring.klx_timesheet.infrastructure.utils.Util.*;
+import static klx.mentoring.klx_timesheet.infrastructure.mappers.BusinessUnitMapper.*;
 
 @Repository
 public class BusinessRepository implements BusinessUnitRepositoryPort {
@@ -44,8 +41,9 @@ public class BusinessRepository implements BusinessUnitRepositoryPort {
     @Override
     public BusinessUnit create(BusinessUnit businessUnit) throws NotFoundCollaboratorException {
         BusinessUnityEntity businessUnityEntity = new BusinessUnityEntity();
-        Set<CollaboratorEntity> collaboratorEntities = businessUnit.collaborators().stream()
-                .map(c -> collaboratorRecordtoEntity(c)).collect(Collectors.toSet());
+        List<UUID> collaboratorIds = businessUnit.collaborators().stream().
+            map(c -> c.id()).collect(Collectors.toList());
+        Set<CollaboratorEntity> collaboratorEntities = getCollaboratorEntities(collaboratorIds);
         businessUnityEntity.setCollaborators(collaboratorEntities);
         businessUnityEntity.setName(businessUnit.name());
         return businessUnityEntityToRecord(businessUnityRepository.save(businessUnityEntity));
@@ -53,12 +51,14 @@ public class BusinessRepository implements BusinessUnitRepositoryPort {
 
     @Override
     public Optional<BusinessUnit> update(BusinessUnit businessUnit, UUID id) throws NotFoundCollaboratorException {
-        Set<CollaboratorEntity> validCollaborators = getValidCollaborators(businessUnit.collaborators());
+        List<UUID> collaboratorIds = businessUnit.collaborators().stream().
+            map(c -> c.id()).collect(Collectors.toList());
+        Set<CollaboratorEntity> collaboratorEntities = getCollaboratorEntities(collaboratorIds);
 
         return businessUnityRepository.findById(id)
                 .map(businessUnitEntity -> {
                     businessUnitEntity.setName(businessUnit.name());
-                    businessUnitEntity.setCollaborators(validCollaborators);
+                    businessUnitEntity.setCollaborators(collaboratorEntities);
                     BusinessUnityEntity savedEntity = businessUnityRepository.save(businessUnitEntity);
                     return businessUnityEntityToRecord(savedEntity);
                 });
@@ -73,8 +73,7 @@ public class BusinessRepository implements BusinessUnitRepositoryPort {
     public Optional<BusinessUnit> addCollaborators(List<UUID> collaboratorIds, UUID id) {
         return this.businessUnityRepository.findById(id)
                 .map(businessUnitEntity -> {
-                    Set<CollaboratorEntity> collaboratorEntities = this.collaboratorRepository
-                            .findByIdIn(collaboratorIds).stream().collect(Collectors.toSet());
+                    Set<CollaboratorEntity> collaboratorEntities = getCollaboratorEntities(collaboratorIds);
                     businessUnitEntity.addCollaborators(collaboratorEntities);
                     return businessUnityEntityToRecord(this.businessUnityRepository.save(businessUnitEntity));
                 });
@@ -84,39 +83,16 @@ public class BusinessRepository implements BusinessUnitRepositoryPort {
     public Optional<BusinessUnit> removeCollaborators(List<UUID> collaboratorIds, UUID id) {
         return this.businessUnityRepository.findById(id)
                 .map(businessUnitEntity -> {
-                    Set<CollaboratorEntity> collaboratorEntities = this.collaboratorRepository
-                            .findByIdIn(collaboratorIds).stream().collect(Collectors.toSet());
+                    Set<CollaboratorEntity> collaboratorEntities = getCollaboratorEntities(collaboratorIds);
                     businessUnitEntity.removeCollaborators(collaboratorEntities);
                     return businessUnityEntityToRecord(this.businessUnityRepository.save(businessUnitEntity));
                 });
     }
 
-    private Set<CollaboratorEntity> getValidCollaborators(Set<Collaborator> collaborators) {
-        StringBuilder missingCollaborators = new StringBuilder();
-
-        // Filtrar colaboradores válidos y acumular los faltantes
-        Set<CollaboratorEntity> validCollaborators = collaborators.stream()
-                .map(collaborator -> {
-                    Optional<CollaboratorEntity> optionalCollaborator = collaboratorRepository
-                            .findById(collaborator.id());
-                    if (optionalCollaborator.isEmpty()) {
-                        missingCollaborators.append(collaborator.id()).append(", ");
-                        return null; // Retornar nulo para colaboradores faltantes
-                    }
-                    return optionalCollaborator.get();
-                })
-                .filter(Objects::nonNull) // Filtrar los nulos
-                .collect(Collectors.toSet());
-
-        // Si hay colaboradores faltantes, lanzar una excepción con todos los IDs
-        if (missingCollaborators.length() > 0) {
-            // Eliminar la última coma y espacio
-            missingCollaborators.setLength(missingCollaborators.length() - 2);
-
-            throw new NotFoundCollaboratorException("Collaborators not found in the database: " + missingCollaborators);
-        }
-
-        return validCollaborators;
+    private Set<CollaboratorEntity> getCollaboratorEntities(List<UUID> collaboratorIds) {
+        Set<CollaboratorEntity> collaboratorEntities = this.collaboratorRepository
+                .findByIdIn(collaboratorIds).stream().collect(Collectors.toSet());
+        return collaboratorEntities;
     }
 
 }
