9a60e979172c74483048c15efe5d161096efeb0d
orlando-santos <santos.orlando.martin@gmail.com>
2025-02-23 12:15:36 +0000

    :wrench: Add @ConfigurationParameter

--- src/test/java/klx/mentoring/klx_timesheet/stepdefinitions/RunCucumberTest.java
+++ src/test/java/klx/mentoring/klx_timesheet/stepdefinitions/RunCucumberTest.java
@@ -1,9 +1,12 @@
 package klx.mentoring.klx_timesheet.stepdefinitions;
 
 import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -11,5 +14,6 @@ import org.junit.platform.suite.api.Suite;
         glue = "klx.mentoring.klx_timesheet.stepdefinitions" // Ruta a tus definiciones de pasos
 )
 @SelectPackages("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
